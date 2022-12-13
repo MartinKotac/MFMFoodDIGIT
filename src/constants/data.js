0e364345e5ab29a0881939752df107a4fea7117a
MartinKotac <you@example.com>
2022-12-13 17:08:34 +0100

    Initial design and components

---
+++ src/constants/data.js
@@ -0,0 +1,60 @@
+import images from './images';
+
+const wines = [
+  {
+    title: 'Chapel Hill Shiraz',
+    price: '$56',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Catena Malbee',
+    price: '$59',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'La Vieillw Rose',
+    price: '$44',
+    tags: 'FR | 750 ml',
+  },
+  {
+    title: 'Tikves Special Edition',
+    price: '$31',
+    tags: 'MK | 750 ml',
+  },
+  {
+    title: 'Fic chardonnay',
+    price: '$26',
+    tags: 'MK | 750 ml',
+  },
+];
+
+const seaFood = [
+  {
+    title: 'Squids',
+    price: '$16',
+    tags: 'Fried | Grilled',
+  },
+  {
+    title: "Octopus",
+    price: '$18',
+    tags: 'Grilled',
+  },
+  {
+    title: 'Mussels',
+    price: '$20',
+    tags: 'Steamed | Fried ',
+  },
+  {
+    title: 'Trout',
+    price: '$30',
+    tags: 'Grilled',
+  },
+  {
+    title: 'Salmon',
+    price: '$40',
+    tags: 'Grilled | Smoked',
+  },
+];
+
+
+export default { wines, seaFood };
---
+++ src/constants/images.js
@@ -0,0 +1,30 @@
+import bg from '../assets/bg.png';
+import mfmLogo from '../assets/mfm-logo-removebg-preview.png';
+import mfmLogoStoOdgovara from '../assets/mfm-odgovara.png'
+import gallery01 from '../assets/gallery01.png';
+import gallery02 from '../assets/gallery02.png';
+import gallery03 from '../assets/gallery03.png';
+import gallery04 from '../assets/gallery04.png';
+import logo from '../assets/logo.png';
+import menu from '../assets/wine.png';
+import overlaybg from '../assets/overlaybg.png';
+import spoon from '../assets/spoon.svg';
+import welcome from '../assets/trout.webp';
+import findus from '../assets/8-6-octopus.jpg';
+
+
+export default {
+  bg,
+  mfmLogo,
+  mfmLogoStoOdgovara,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
+  logo,
+  menu,
+  overlaybg,
+  spoon,
+  welcome,
+  findus
+};
